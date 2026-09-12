0d562719f1904495a8a6aa343fb40dc81f0a7abb
atul-aiml <atulchoudhary3213@example.com>
2026-09-12 17:14:11 +0530

    Data Validation Completed

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,10 +1,11 @@
+import certifi
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://atulchoudhary3213_db_user:Atuljaat123@cluster0.sjktvjz.mongodb.net/?appName=Cluster0"
+uri = "mongodb+srv://atulchoudhary3213_db_user:9XcwvYT4rRtrEJNF@cluster0.sjktvjz.mongodb.net/?appName=Cluster0"
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri, server_api=ServerApi('1'),tlsCAFile=certifi.where())
 
 # Send a ping to confirm a successful connection
 try:
