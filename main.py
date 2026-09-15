994af7f36f4db27f903be49d6ef850b1c82163a9
atul-aiml <atulchoudhary3213@example.com>
2026-09-15 12:54:08 +0530

    Trainer evaluration and mlflow commited

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataTransformationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.entity.config_entity import DataValidationConfig
 
@@ -31,5 +32,15 @@ if __name__ == '__main__':
         print(data_transformation_artifact)
         logging.info("data Transformation completed.")
         
+        logging.info("Model Training sstarted")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(
+            model_trainer_config=model_trainer_config,
+            data_transformation_artifact=data_transformation_artifact,
+        )
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
